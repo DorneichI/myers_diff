65571350bd0acd782c3119e34d33bb431bb72277
Immanuel Dorneich <immanuel.dorneich@gmail.com>
2025-09-28 21:00:01 -0400

    removed unnecessary print

--- main.c
+++ main.c
@@ -23,25 +23,7 @@ int main(int argc, char *argv[]) {
 
   tracks *track = backtrack(size_a, size_b, trace);
 
-  for (int i = 0; i < track->size * 2 - 1; i += 2) {
-    printf("x: %i, y: %i\n", track->data[i], track->data[i + 1]);
-  }
-
   diffs *line_diff_result = create_diffs(track, lines_a, lines_b);
-  diff *data = line_diff_result->data;
-
-  for (int i = 0; i < line_diff_result->size; i++) {
-    if (data[i].action == INSERT) {
-      printf("INSERT\n");
-      printf("%s\n", data[i].line_b);
-    } else if (data[i].action == DELETE) {
-      printf("DELETE\n");
-      printf("%s\n", data[i].line_a);
-    } else {
-      printf("EQUAL\n");
-      printf("%s\n", data[i].line_b);
-    }
-  }
 
   print_diff(line_diff_result);
 
